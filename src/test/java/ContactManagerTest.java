066df6b96d26599930ab27bdd4bc86e6d979adff
Nickolay Levotsky <nickolaykla@gmail.com>
2022-05-23 17:58:37 +0300

    adding some other tests

--- src/test/java/ContactManagerTest.java
+++ src/test/java/ContactManagerTest.java
@@ -1,4 +1,6 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,5 +19,23 @@ public class ContactManagerTest {
         contactManager.addContact("Vasiliy", "Petrov", "375445341232");
         assertFalse(contactManager.getAllContacts().isEmpty());
         assertEquals(1, contactManager.getAllContacts().size());
+    }
+    @Test
+    public void checkIfFirstNameIsNull() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact(null, "Petrov", "375445341232");
+        });
+    }
+    @Test
+    public void checkIfLastNameisNull() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("Vasiliy", null, "375445341232");
+        });
+    }
+    @Test
+    public void checkIfNumberIsNull() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("Vasiliy", "Petrov", null);
+        });
     }
 }
